b472e993f0848963b2ca0cebc18f780e019bb8dc
Luis Macias <luismacias.angulo@gmail.com>
2016-11-18 19:55:18 -0600

    Inicio proyecto con Yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ocoAppApp
+ * @description
+ * # ocoAppApp
+ *
+ * Main module of the application.
+ */
+angular
+	.module('ocoAppApp', [
+		'ngAnimate'
+	]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ocoAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ocoAppApp
+ */
+angular.module('ocoAppApp')
+	.controller('MainCtrl', function () {
+		this.awesomeThings = [
+			'HTML5 Boilerplate',
+			'AngularJS',
+			'Karma'
+		];
+	});
